3f2c67d9bd279946cf505012eeae3e0b19cad939
DLMcCoy88 <48062956+DLMcCoy88@users.noreply.github.com>
2019-12-04 07:06:23 -0800

    Add files via upload

--- readings.php
+++ readings.php
@@ -56,8 +56,6 @@
 			<li><a href="layout.php"> LAYOUT </a></li>
 			<li><a href="about.php"> ABOUT </a></li>
 			<li class ="active"><a href="readings.php"> READINGS </a></li>
-			<li><a href="employee info.php"> EMPOYEE INFO </a></li>
-
 
 		</ul> 
 
